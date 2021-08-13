0726af4372298f572cd02c484ee948563e403373
jeffbeagley <jeffbeagley@gmail.com>
2021-08-13 15:28:52 -0500

    add sleep script to wait for db to be bootstrapped before installing

--- docker/install-drupal.sh
+++ docker/install-drupal.sh
@@ -4,6 +4,9 @@ echo 'installing drupal'
 
 cd /var/www/public_html
 
+# wait for database to bootstrap itself before we attempt to run the install command
+sleep 10
+
 drush --root=/var/www/public_html site-install standard -vv --account-name=admin --account-pass=admin --yes --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:3306/${MYSQL_DATABASE}
 drush --root=/var/www/public_html theme:enable lark
 drush --root=/var/www/public_html cr
