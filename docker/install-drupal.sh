753c21d01099561c44c642246327e059918c5de1
jeffbeagley <jeffbeagley@gmail.com>
2021-08-27 12:07:31 -0500

    create canned settings.php file that references env vars instead of hard coded values

--- docker/install-drupal.sh
+++ docker/install-drupal.sh
@@ -7,7 +7,7 @@ cd /var/www/public_html
 # wait for database to bootstrap itself before we attempt to run the install command
 sleep 10
 
-drush --root=/var/www/public_html site-install standard -vv --account-name=admin --account-pass=admin --yes --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:3306/${MYSQL_DATABASE}
+drush --root=/var/www/public_html site-install standard -vv --account-name=admin --account-pass=admin --yes
 drush --root=/var/www/public_html theme:enable lark
 drush --root=/var/www/public_html cr
 drush --root=/var/www/public_html config-set system.theme admin lark -y
