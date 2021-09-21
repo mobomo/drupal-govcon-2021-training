ef3b5cd7ae70c63c2f5044d98a4dc70a29ca918a
EC2 Default User <ec2-user@ip-172-31-55-217.ec2.internal>
2021-09-21 23:33:51 +0000

    WIP: refactor

--- step1/docker/entrypoint.sh
+++ step1/docker/entrypoint.sh
@@ -2,9 +2,5 @@
 
 echo 'entrypoint of app'	
 
-cd /var/www/drupal
-
-mv settings.php web/sites/default/
-
 exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf &
 exec /usr/sbin/php-fpm7 -F
