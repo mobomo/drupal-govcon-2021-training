aad191005dd4f8117bfb129f0750b4a201864957
EC2 Default User <ec2-user@ip-172-31-55-217.ec2.internal>
2021-09-22 18:45:42 +0000

    Trying to fix permission issues related to drush

---
+++ step1/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CWD=$(dirname $0)
+
+aws cloudformation create-stack --stack-name phase1-webserver \
+ --template-body file://${PWD}/step1/cloudformation/01-create-ec2-instance.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --parameters ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
+ --region us-east-1
