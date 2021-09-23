6ffe4d8a778a4850e52dc04c1ace422b61841507
Jason Schulte <jason@mobomo.com>
2021-09-23 22:54:58 +0000

    Add options for bash scripts to output commands. Finish upgrade to aurora-mysql5.7

--- step2-rds/04-migrate.sh
+++ step2-rds/04-migrate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
 
