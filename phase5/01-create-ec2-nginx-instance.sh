7f1cadad15691d6621aac2bae416665320d68bd2
jeffbeagley <jeffbeagley@gmail.com>
2021-09-28 13:16:34 -0500

    add nginx configuration for phase 5

--- phase5/01-create-ec2-nginx-instance.sh
+++ phase5/01-create-ec2-nginx-instance.sh
@@ -3,8 +3,8 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-aws cloudformation create-stack --stack-name phase4-elasticache \
- --template-body file://${CWD}/cloudformation/06-elasticache.yaml \
+aws cloudformation create-stack --stack-name phase5-nginx \
+ --template-body file://${CWD}/cloudformation/01-ec2-nginx-instance.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameters ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
  --region us-east-1
