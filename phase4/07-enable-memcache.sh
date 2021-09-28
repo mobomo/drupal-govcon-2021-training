006b24675b3e1ec80a91d4081391d6a617bd58d6
Jason Schulte <jason@mobomo.com>
2021-09-28 16:04:08 +0000

    Create memcache script to enable it

--- phase4/07-enable-memcache.sh
+++ phase4/07-enable-memcache.sh
@@ -6,8 +6,8 @@ set -a && source ${CWD}/.env && set +a
 WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
 
 ssh -o "StrictHostKeyChecking no" $WEBSERVER_IP << EOF
-  docker exec -it govcon2021_web sh -c "cd ${DRUPAL_ROOT} && composer require drupal/memcache"
-  docker exec -it govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} en -y memcache"
-  docker exec -it govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} en -y memcache_admin"
-  docker exec -it govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} cr -y"
+  docker exec govcon2021_web sh -c "cd ${DRUPAL_ROOT} && composer require drupal/memcache"
+  docker exec govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} en -y memcache"
+  docker exec govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} en -y memcache_admin"
+  docker exec govcon2021_web sh -c "drush --root=${DRUPAL_ROOT} cr -y"
 EOF
