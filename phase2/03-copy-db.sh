52dd7a40f4b26ccc1ec19e1724ca2603fcd7e125
Jason Schulte <jason@mobomo.com>
2021-09-28 14:37:39 +0000

    fix settings.php and update ssh commands

--- phase2/03-copy-db.sh
+++ phase2/03-copy-db.sh
@@ -7,4 +7,4 @@ WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver 
 
 echo "CREATE DATABASE ${MYSQL_DATABASE}" | mysql -h db.govcon2021.mobomo.net -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD}
 
-ssh -t $WEBSERVER_IP docker exec -it govcon2021_web drush --root=${DRUPAL_ROOT} sql-dump | pv | mysql -h ${MYSQL_HOST} -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
+ssh -to "StrictHostKeyChecking no" $WEBSERVER_IP docker exec -it govcon2021_web drush --root=${DRUPAL_ROOT} sql-dump | pv | mysql -h ${MYSQL_HOST} -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE}
--- phase2/04-create-db-user.sh
+++ phase2/04-create-db-user.sh
@@ -3,7 +3,6 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -h ${MYSQL_HOST} -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD}
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" | mysql -h ${MYSQL_HOST} -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD}
 echo "FLUSH PRIVILEGES;" | mysql mysql -h ${MYSQL_HOST} -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD}
