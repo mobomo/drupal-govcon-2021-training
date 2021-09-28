0ec6c00150cb907fb717f23d58e707f5b7730912
jeffbeagley <jeffbeagley@gmail.com>
2021-09-28 10:35:27 -0500

    copy files into phase 4

---
+++ phase4/03-create-s3-bucket.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+CWD=$(dirname $0)
+set -a && source $CWD/.env && set +a
+
+aws cloudformation create-stack --stack-name phase3-s3-bucket --template-body file://${CWD}/cloudformation/03-s3-bucket.yaml \
+--parameters \
+ParameterKey=BucketName,ParameterValue=drupal-govcon-2021-assets
---
+++ phase4/06-create-elasticache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+CWD=$(dirname $0)
+set -a && source ${CWD}/.env && set +a
+
+aws cloudformation create-stack --stack-name phase4-elasticache \
+ --template-body file://${CWD}/cloudformation/06-elasticache.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --parameters ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
+ --region us-east-1
