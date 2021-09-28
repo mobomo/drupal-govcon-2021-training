5de9f5316aed536eddb53a0e788a869b5639663c
Jason Schulte <jason@mobomo.com>
2021-09-28 17:38:21 +0000

    fix lark theme stuff

--- phase2/04-migrate.sh
+++ phase2/04-migrate.sh
@@ -5,7 +5,7 @@ set -a && source ${CWD}/.env && set +a
 
 WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
 
-ssh -o "StrictHostKeyChecking no" $WEBSERVER_IP << EOF
+ssh -o "StrictHostKeyChecking=no" $WEBSERVER_IP << EOF
   cd /var/app 
   docker-compose -f phase1/docker-compose.yml down
   docker-compose -f phase2/docker-compose.yml up -d --build
