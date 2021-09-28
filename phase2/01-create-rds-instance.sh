9a323c00936352e07987dc445b38f71b0c7107f2
Jason Schulte <jason@mobomo.com>
2021-09-28 17:20:34 +0000

    renaming stuff

--- phase2/01-create-rds-instance.sh
+++ phase2/01-create-rds-instance.sh
@@ -3,7 +3,7 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-aws cloudformation create-stack --stack-name phase2-rds --template-body file://${CWD}/cloudformation/02-rds-instance.yaml \
+aws cloudformation create-stack --stack-name phase2-rds --template-body file://${CWD}/cloudformation/01-rds-instance.yaml \
 --parameters \
 ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
 ParameterKey=DBInstanceClass,ParameterValue=db.t3.small \
--- phase3/01-create-s3-bucket.sh
+++ phase3/01-create-s3-bucket.sh
@@ -3,6 +3,6 @@
 CWD=$(dirname $0)
 set -a && source $CWD/.env && set +a
 
-aws cloudformation create-stack --stack-name phase3-s3-bucket --template-body file://${CWD}/cloudformation/03-s3-bucket.yaml \
+aws cloudformation create-stack --stack-name phase3-s3-bucket --template-body file://${CWD}/cloudformation/01-s3-bucket.yaml \
 --parameters \
 ParameterKey=BucketName,ParameterValue=drupal-govcon-2021-assets
