716f4abe5512354aaf07a13dd5674393782764fd
jeffbeagley <jeffbeagley@gmail.com>
2021-09-28 15:43:23 -0500

    add public ip and dns creation to cf stack for phase 5

--- phase5/01-create-ec2-nginx-instance.sh
+++ phase5/01-create-ec2-nginx-instance.sh
@@ -3,8 +3,11 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
+WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePublicIP") | .OutputValue')
+
 aws cloudformation create-stack --stack-name phase5-nginx \
  --template-body file://${CWD}/cloudformation/01-ec2-nginx-instance.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameters ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
+ --parameters ParameterKey=WebServerPublicIP,ParameterValue=${WEBSERVER_IP} \
  --region us-east-1
