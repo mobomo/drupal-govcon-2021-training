a5c290edd8d1800ca1a73c3d24d52e65a411cbbf
Jason Schulte <jason@mobomo.com>
2021-09-29 23:27:29 +0000

    reorder commands for s3fs enable script

--- phase3/02-enable-s3fs.sh
+++ phase3/02-enable-s3fs.sh
@@ -6,8 +6,8 @@ set -a && source ${CWD}/.env && set +a
 WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
 
 ssh $WEBSERVER_IP << EOF
-  docker exec -u http govcon2021_web sh -c "drush pm-enable s3fs && drush cache-rebuild && drush s3fs-copy-local"
   cd /var/app 
   docker-compose -f phase2/docker-compose.yml down
   docker-compose -f phase3/docker-compose.yml up -d --build
+  docker exec -u http govcon2021_web sh -c "drush pm-enable s3fs && drush cache-rebuild && drush s3fs-copy-local"
 EOF
