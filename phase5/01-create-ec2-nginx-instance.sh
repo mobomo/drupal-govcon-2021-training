4a9e1bf47575888d1be7bca74be9bb7e7247a84c
Jason Schulte <jason@mobomo.com>
2021-09-29 17:57:49 +0000

    WIP: reverse proxy stuff

--- phase5/01-create-ec2-nginx-instance.sh
+++ phase5/01-create-ec2-nginx-instance.sh
@@ -3,12 +3,14 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-WEBSERVER_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePublicIP") | .OutputValue')
+WEBSERVER_PRIVATE_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
+WEBSERVER_PUBLIC_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePublicIP") | .OutputValue')
 
 aws cloudformation create-stack --stack-name phase5-nginx \
  --template-body file://${CWD}/cloudformation/01-ec2-nginx-instance.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameters \
  ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
- ParameterKey=WebServerPublicIP,ParameterValue=${WEBSERVER_IP} \
+ ParameterKey=WebServerPublicIP,ParameterValue=${WEBSERVER_PUBLIC_IP} \
+ ParameterKey=WebServerPrivateIP,ParameterValue=${WEBSERVER_PRIVATE_IP} \
  --region us-east-1
