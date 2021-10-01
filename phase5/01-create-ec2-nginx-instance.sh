92ac89573076aa1e9491d2e6dbdb63d14bc92980
Jason Schulte <jason@mobomo.com>
2021-10-01 22:58:46 +0000

    fix phase5 deploy script

--- phase5/01-create-ec2-nginx-instance.sh
+++ phase5/01-create-ec2-nginx-instance.sh
@@ -4,7 +4,7 @@ CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
 WEBSERVER_PRIVATE_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
-WEBSERVER_PUBLIC_DNS_NAME=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="Instance") | .OutputValue')
+WEBSERVER_PUBLIC_DNS_NAME=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePublicDNSName") | .OutputValue')
 
 aws cloudformation create-stack --stack-name phase5-nginx \
  --template-body file://${CWD}/cloudformation/01-ec2-nginx-instance.yaml \
