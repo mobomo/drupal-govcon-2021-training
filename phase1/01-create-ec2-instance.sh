594a3e774a95ec65a6abf922adf63f23c22cdd6d
jeffbeagley <jeffbeagley@gmail.com>
2021-10-01 14:46:54 -0500

    update phase 1 to deploy type command

--- phase1/01-create-ec2-instance.sh
+++ phase1/01-create-ec2-instance.sh
@@ -3,8 +3,9 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-aws cloudformation create-stack --stack-name phase1-webserver \
- --template-body file://${CWD}/cloudformation/01-ec2-instance.yaml \
+aws cloudformation deploy --stack-name phase1-webserver \
+ --template-file ${CWD}/cloudformation/01-ec2-instance.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
- --parameters ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
+ --parameter-overrides \
+ 	DefaultSecurityGroupId=sg-af2496b0 \
  --region us-east-1
