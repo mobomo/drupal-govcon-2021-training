45f4583be6ef0e079550419d528be0f0b411eb74
jeffbeagley <jeffbeagley@gmail.com>
2021-10-01 14:52:34 -0500

    swap to using cnames for www and cms records

--- phase5/01-create-ec2-nginx-instance.sh
+++ phase5/01-create-ec2-nginx-instance.sh
@@ -4,13 +4,13 @@ CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
 WEBSERVER_PRIVATE_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePrivateIP") | .OutputValue')
-WEBSERVER_PUBLIC_IP=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="InstancePublicIP") | .OutputValue')
+WEBSERVER_PUBLIC_DNS_NAME=$(aws cloudformation describe-stacks --stack-name phase1-webserver | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="Instance") | .OutputValue')
 
 aws cloudformation create-stack --stack-name phase5-nginx \
  --template-body file://${CWD}/cloudformation/01-ec2-nginx-instance.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameters \
  ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
- ParameterKey=WebServerPublicIP,ParameterValue=${WEBSERVER_PUBLIC_IP} \
+ ParameterKey=WebServerPublicDnsName,ParameterValue=${WEBSERVER_PUBLIC_DNS_NAME} \
  ParameterKey=WebServerPrivateIP,ParameterValue=${WEBSERVER_PRIVATE_IP} \
  --region us-east-1
