ea38fd8b64c0bd7d33fa157325c89eafa6b43efc
jeffbeagley <jeffbeagley@gmail.com>
2021-10-01 14:55:24 -0500

    Update cms and www dns records with alb values from phase6

--- phase6/01-create-autoscaling-groups.sh
+++ phase6/01-create-autoscaling-groups.sh
@@ -3,16 +3,68 @@
 CWD=$(dirname $0)
 set -a && source ${CWD}/.env && set +a
 
-# aws cloudformation create-stack --stack-name phase6-autoscaling-group-web \
-#  --template-body file://${CWD}/cloudformation/01-web-autoscale-group.yaml \
-#  --capabilities CAPABILITY_NAMED_IAM \
-#  --parameters \
-#  	ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
-#  --region us-east-1
-
-aws cloudformation create-stack --stack-name phase6-autoscaling-group-nginx \
- --template-body file://${CWD}/cloudformation/02-nginx-autoscale-group.yaml \
+## Deploy Drupal into an auto scaling group
+aws cloudformation deploy --stack-name phase6-autoscaling-group-web \
+ --template-file ${CWD}/cloudformation/01-web-autoscale-group.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
- --parameters \
+ --parameter-overrides \
  	ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
- --region us-east-1
+ --region us-east-1 \
+ --no-fail-on-empty-changeset
+
+## Deploy Nginx into an auto scaling group
+aws cloudformation deploy --stack-name phase6-autoscaling-group-nginx \
+ --template-file ${CWD}/cloudformation/02-nginx-autoscale-group.yaml \
+ --capabilities CAPABILITY_NAMED_IAM \
+ --parameter-overrides \
+ 	ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
+ --region us-east-1 \
+ --no-fail-on-empty-changeset
+ 
+## Get DNS values from stacks
+DRUPAL_ALB_DNS=$(aws cloudformation describe-stacks --stack-name phase6-autoscaling-group-web | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="ALBDNSValue") | .OutputValue')
+NGINX_ALB_DNS=$(aws cloudformation describe-stacks --stack-name phase6-autoscaling-group-nginx | jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="ALBDNSValue") | .OutputValue')
+
+## Update Route53 record for cms hostname
+aws route53 change-resource-record-sets --hosted-zone-id Z01629752800X7928YLRP --change-batch file://<(cat << EOF
+{
+  "Changes": [
+    {
+      "Action": "DELETE",
+      "ResourceRecordSet": {
+        "Name": "cms.govcon2021.mobomo.net",
+        "Type": "CNAME",
+        "TTL": 120,
+        "ResourceRecords": [
+          {
+            "Value": "${DRUPAL_ALB_DNS}"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+)
+
+## Update Route53 record for www hostname
+aws route53 change-resource-record-sets --hosted-zone-id Z01629752800X7928YLRP --change-batch file://<(cat << EOF
+{
+  "Changes": [
+    {
+      "Action": "DELETE",
+      "ResourceRecordSet": {
+        "Name": "www.govcon2021.mobomo.net",
+        "Type": "CNAME",
+        "TTL": 120,
+        "ResourceRecords": [
+          {
+            "Value": "${NGINX_ALB_DNS}"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+)
