bc7613b013f13af24cfc1f0c266e8da23e45affe
Jason Schulte <jason@mobomo.com>
2021-10-05 19:26:49 +0000

    rename files

--- phase6/01-create-autoscaling-groups.sh
+++ phase6/01-create-autoscaling-groups.sh
@@ -13,7 +13,7 @@ aws cloudformation create-stack --stack-name phase6-autoscaling-group-web \
 
 ## Deploy Nginx into an auto scaling group
 aws cloudformation create-stack --stack-name phase6-autoscaling-group-nginx \
- --template-body file://${CWD}/cloudformation/02-nginx-autoscale-group.yaml \
+ --template-body file://${CWD}/cloudformation/02-rproxy-autoscale-group.yaml \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameters \
     ParameterKey=DefaultSecurityGroupId,ParameterValue=sg-af2496b0 \
